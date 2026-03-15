9e9766896696ae94495cd8c8327429f34bdb324f
Fidelis Emus <fidelisemus@gmail.com>
2026-03-15 02:32:33 +0100

    Update database path to use persistent volume

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const db = new Database("trustline.db");
+const db = new Database("/app/data/trustline.db");
 
 // Initialize Database
 db.exec(`
